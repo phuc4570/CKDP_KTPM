fd9f1660bb4f583df6e7f97035283e86646a9852
Pal Nguyen <pnpal2002@gmail.com>
2023-05-02 11:47:23 +0700

    feat: register page

--- components/admin/index.js
+++ components/admin/index.js
@@ -1,15 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const edit_accounts = require('./edit_accounts/accountsController');
-const edit_products = require('./edit_products/edit_productsController');
-const profile = require('./profile/profileController');
-const statistic = require('./statistic/statisticController');
+const edit_accounts = require("./edit_accounts/accountsController");
+const edit_products = require("./edit_products/edit_productsController");
+const profile = require("./profile/profileController");
+const statistic = require("./statistic/statisticController");
+const register = require("./register/registerController");
 
-router.get('/', profile.profile);
-router.get('/edit_accounts', edit_accounts.account);
-router.get('/edit_products', edit_products.edit_products);
-router.get('/profile', profile.profile);
-router.get('/statistic', statistic.statistic);
+router.get("/", profile.profile);
+router.get("/edit_accounts", edit_accounts.account);
+router.get("/edit_products", edit_products.edit_products);
+router.get("/profile", profile.profile);
+router.get("/statistic", statistic.statistic);
+router.get("/register", register.register);
 
 module.exports = router;
---
+++ components/admin/register/registerController.js
@@ -0,0 +1,9 @@
+const globalVar = require("../../../routes/globalVar");
+
+exports.register = (req, res) => {
+  if (isLogin !== 1) {
+    if (isLogin === 2) res.redirect("/user");
+    res.redirect("/");
+  }
+  res.render("admin/register/register", { layout: "admin_layout" });
+};
