9ce04c61f354002ff99fe2dd25476d55ab380fca
Pal Nguyen <pnpal2002@gmail.com>
2023-05-02 11:46:11 +0700

    fix: cartController, admin_layout, user_layout

---
+++ components/user/cart/cartController.js
@@ -0,0 +1,9 @@
+const globalVar = require("../../../routes/globalVar");
+
+exports.cart = (req, res) => {
+  if (isLogin !== 2) {
+    if (isLogin === 1) res.redirect("/admin");
+    res.redirect("/");
+  }
+  res.render("user/cart/cart", { layout: "user_layout" });
+};
