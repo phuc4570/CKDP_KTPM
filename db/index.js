0d3a6802c8024c774fce38ae2a6396ba60fcfde3
Phúc Nguyễn <phuc4570@gmail.com>
2023-05-10 21:30:01 +0700

    fix: change database server

--- db/index.js
+++ db/index.js
@@ -5,10 +5,10 @@ const db = { connection: null };
 
 (async () => {
   db.connection = await mysql.createConnection({
-    host: "sql.freedb.tech",
-    user: "freedb_ababa",
-    password: "wA8&94c!m7eNGa?",
-    database: "freedb_NetCafe",
+    host: "db4free.net",
+    user: "adminxcdp",
+    password: "12345678",
+    database: "netcafexcdp",
   });
   console.log("Database connected!");
 })();
