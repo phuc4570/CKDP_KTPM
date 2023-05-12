aa34a9c16b08e7fe1979d521c7220acf70792856
Phúc Nguyễn <phuc4570@gmail.com>
2023-05-12 23:49:55 +0700

    Merge branch 'main' of https://github.com/palnguyen20-01/PTUDW-CDP

--- public/assets_editproducts/js/main.js
+++ public/assets_editproducts/js/main.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
-    function fetchproducts(page, size, category, namesorting, desc){
+    function fetchproducts(page, size, category, namesorting, pricesorting, desc){
         let pageNumber = (typeof page !== 'undefined') ?  page : 0;
         let sizeNumber = (typeof size !== 'undefined') ?  size : 5;
         let selectedcategory = (typeof category !== 'undefined') ?  category : -1;
         let nameSorted = (typeof namesorting !== 'undefined') ?  namesorting: false;
+        let priceSorted = (typeof pricesorting !== 'undefined') ?  pricesorting: false;
         let descDirection = (typeof desc !== 'undefined') ?  desc: false;
 
         /**
@@ -17,6 +18,7 @@ $(document).ready(function(){
                 size: sizeNumber,
                 category: selectedcategory,
                 namesorting: nameSorted,
+                pricesorting: priceSorted,
                 desc: descDirection
             },
             success: function(response){
@@ -37,7 +39,7 @@ $(document).ready(function(){
 
 
 
-                if ($('ul.pagination li').length - 2 != response.totalPages){
+                if ($('ul.pagination li').length - 4 != response.totalPages){
                     // build pagination list at the first time loading
                     $('ul.pagination').empty();
                     buildPagination(response.totalPages);
@@ -58,13 +60,18 @@ $(document).ready(function(){
 
 
         category = this.value;
-        
+
 
         let namesorting = false;
+        let pricesorting = false;
         let desc = false;
 
         if($("#name_sorting"). prop("checked") == true){
             namesorting = true;
+        }
+
+        if($("#price_sorting"). prop("checked") == true){
+            pricesorting = true;
         }
 
         if($("#desc_sorting"). prop("checked") == true){
@@ -72,7 +79,7 @@ $(document).ready(function(){
         }
 
         // re-fetch customer list again
-        fetchproducts(0, 5, category, namesorting, desc);
+        fetchproducts(0, 5, category, namesorting, pricesorting, desc);
     });
 
     /**
@@ -99,7 +106,7 @@ $(document).ready(function(){
     }
 
     /**
-     * age_sorting checkbox is changed
+     * name_sorting checkbox is changed
      */
     $('#name_sorting').on('change', function() {
         if(this.checked){
@@ -112,11 +119,22 @@ $(document).ready(function(){
         }
     });
 
+    $('#price_sorting').on('change', function() {
+        if(this.checked){
+            $("#desc_sorting").removeAttr("disabled");
+            $("#sortingbtn").removeAttr("disabled");
+        }else {
+            $("#desc_sorting").attr("disabled", true);
+            $("#desc_sorting").prop("checked", false);
+            $("#sortingbtn").attr("disabled", true);
+        }
+    });
     /**
      * Click on sorting Button
      */
     $(document).on("click", "#sortingbtn", function() {
         let namesorting = false;
+        let pricesorting = false;
         let desc = false;
         let selectedCategory = getSeletedCategory();
 
@@ -127,6 +145,10 @@ $(document).ready(function(){
             namesorting = true;
         }
 
+        if($("#price_sorting"). prop("checked") == true){
+            pricesorting = true;
+        }
+
         /* desc checkbox */
         if($("#desc_sorting"). prop("checked") == true){
             desc = true;
@@ -134,10 +156,13 @@ $(document).ready(function(){
 
         // get the active index of pagination bar
         let selectedPageIndex = parseInt($("ul.pagination li.active").text()) - 1;
-
-        // just fetch again products from SpringBoot RestAPIs when namesorting checkbox is checked
+        console.log(selectedPageIndex);
+        // just fetch again products from SpringBoot RestAPIs when sorting checkbox is checked
         if(namesorting){
-            fetchproducts(selectedPageIndex, 5, selectedCategory, namesorting, desc); // get next page value
+            fetchproducts(selectedPageIndex, 5, selectedCategory, namesorting, pricesorting, desc); // get next page value
+        }
+        if(pricesorting){
+            fetchproducts(selectedPageIndex, 5, selectedCategory, namesorting, pricesorting, desc); // get next page value
         }
     });
 
@@ -147,13 +172,16 @@ $(document).ready(function(){
      */
     function buildPagination(totalPages){
         // Build paging navigation
+        let firstPage = '<li class="page-item"><a class="page-link">First</a></li>';
         let pageIndex = '<li class="page-item"><a class="page-link">Previous</a></li>';
+        $("ul.pagination").append(firstPage);
         $("ul.pagination").append(pageIndex);
 
         // create pagination
         for(let i=1; i <= totalPages; i++){
             // adding .active class on the first pageIndex for the loading time
             if(i==1){
+
                 pageIndex = "<li class='page-item active'><a class='page-link'>"
                     + i + "</a></li>"
             } else {
@@ -162,9 +190,10 @@ $(document).ready(function(){
             }
             $("ul.pagination").append(pageIndex);
         }
-
+        let lastPage = '<li class="page-item last"><a class="page-link">Last</a></li>';
         pageIndex = '<li class="page-item"><a class="page-link">Next</a></li>';
         $("ul.pagination").append(pageIndex);
+        $("ul.pagination").append(lastPage);
     }
 
     /**
@@ -192,10 +221,14 @@ $(document).ready(function(){
      */
     $(document).on("click", "ul.pagination li a", function() {
         let namesorting = false;
+        let pricesorting = false;
         let desc = false;
         let selectedCategory = getSeletedCategory();
         if($("#name_sorting"). prop("checked") == true){
             namesorting = true;
+        }
+        if($("#price_sorting"). prop("checked") == true){
+            pricesorting = true;
         }
 
         if($("#desc_sorting"). prop("checked") == true){
@@ -207,10 +240,10 @@ $(document).ready(function(){
         // click on the NEXT tag
         if(val.toUpperCase()==="NEXT"){
             let activeValue = parseInt($("ul.pagination li.active").text());
-            let totalPages = $("ul.pagination li").length - 2; // -2 beacause 1 for Previous and 1 for Next
+            let totalPages = $("ul.pagination li").length - 4; // -2 beacause 1 for Previous and 1 for Next
             if(activeValue < totalPages){
                 let currentActive = $("li.active");
-                fetchproducts(activeValue, 5, selectedCategory, namesorting, desc); // get next page value
+                fetchproducts(activeValue, 5, selectedCategory, namesorting, pricesorting, desc); // get next page value
                 // remove .active class for the old li tag
                 $("li.active").removeClass("active");
                 // add .active to next-pagination li
@@ -220,17 +253,33 @@ $(document).ready(function(){
             let activeValue = parseInt($("ul.pagination li.active").text());
             if(activeValue > 1){
                 // get the previous page
-                fetchproducts(activeValue-2, 5, selectedCategory, namesorting, desc);
+                fetchproducts(activeValue-2, 5, selectedCategory, namesorting, pricesorting, desc);
                 let currentActive = $("li.active");
                 currentActive.removeClass("active");
                 // add .active to previous-pagination li
                 currentActive.prev().addClass("active");
             }
-        } else {
-            fetchproducts(parseInt(val) - 1, 5,  selectedCategory, namesorting, desc);
+        } else if(val.toUpperCase()==="FIRST") {
+            fetchproducts(0, 5,  selectedCategory, namesorting, pricesorting, desc);
+            $("li.active").removeClass("active");
+
+            $(".carousel-inner .carousel-item:first-child").addClass("active");
+        } else if(val.toUpperCase()==="LAST") {
+            let page = $("ul.pagination li").length -4;
+            fetchproducts(page -1, 5,  selectedCategory, namesorting, pricesorting, desc);
+            $("li.active").removeClass("active");
+            $(".carousel-inner .carousel-item:last-child").addClass("active");
+        }
+        else {
+            fetchproducts(parseInt(val) - 1, 5,  selectedCategory, namesorting, pricesorting, desc);
             // add focus to the li tag
             $("li.active").removeClass("active");
             $(this).parent().addClass("active");
         }
     });
+
+});
+
+$('input[type="checkbox"]').on('change', function() {
+    $('input[name="' + this.name + '"]').not(this).prop('checked', false);
 });
